3d0f2fa7d36e9422a55dce08625b88a5b56b9bae
albinsony95 <albinsony8@gmail.com>
2023-10-15 20:46:23 -0600

    most of the media queries done

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -4,26 +4,25 @@ let arrayshow=[{date:"Mon Sept 06 2021",venue:"Ronald Lane",location:"San Franci
                 {date:"Sat Nov 06 2021 ",venue:"Hyatt Agency",location:"San Francisco, CA"},
                 {date:"Fri Nov 26 2021",venue:"Moscow Center",location:"San Francisco, CA"},
                 {date:"Wed Dec 15 2021",venue:"Press Club",location:"San Francisco, CA"}];
-displayshow(arrayshow);
+display(arrayshow);
 
-function displayshow(arr){
-    console.log(arr);
+function display(arrr){
     mainbox=document.getElementById('showBox');
-let headdate=document.createElement('td');
+let headdate=document.createElement('p');
 setAttributes(headdate,{"class":"showMain__show__box__table__headrow--heading"});
 headdate.innerHTML="DATE";
-let headvenue=document.createElement('td');
+let headvenue=document.createElement('p');
 setAttributes(headvenue,{"class":"showMain__show__box__table__headrow--heading"});
 headvenue.innerHTML="VENUE";
-let headlocation=document.createElement('td');
+let headlocation=document.createElement('p');
 setAttributes(headlocation,{"class":"showMain__show__box__table__headrow--heading"});
 headlocation.innerHTML="LOCATION";
 
-let headrow=document.createElement('tr');
+let headrow=document.createElement('div');
 setAttributes(headrow,{"class":"showMain__show__box__table__headrow"});
 console.log(headdate.innerHTML);
 
-let showtable=document.createElement('table');
+let showtable=document.createElement('article');
 setAttributes(showtable,{"class":"showMain__show__box__table"});
 showtable.appendChild(headrow);
 headrow.appendChild(headdate);
@@ -34,26 +33,26 @@ headrow.appendChild(headlocation);
 showtable.appendChild(headrow);
 
 
-for(i=0;i<arr.length;i++){
+for(i=0;i<arrr.length;i++){
     // console.log(arr);
-    ticket=arr[i];
+    ticket=arrr[i];
     // console.log(ticket.date);
-    let rows=document.createElement('tr');
-    setAttributes(rows,{"class":"showMain__show__box__table__datarow"});
+    let rows=document.createElement('article');
+    setAttributes(rows,{"class":"showMain__show__box__table__datarow","id":"selecthover"});
 
-    let date=document.createElement('td');
+    let date=document.createElement('p');
     setAttributes(date,{"class":"showMain__show__box__table__datarow__date"});
     date.innerHTML=ticket.date;
     // console.log(date);
-    let venue=document.createElement('td');
+    let venue=document.createElement('p');
     setAttributes(venue,{"class":"showMain__show__box__table__datarow__venue"});
     venue.innerHTML=ticket.venue;
-    let location=document.createElement('td');
+    let location=document.createElement('p');
     setAttributes(location,{"class":"showMain__show__box__table__datarow__location"});
     location.innerHTML=ticket.location;
     let buybutton=document.createElement('button');
-    setAttributes(buybutton,{"class":"showMain__show__box__table__datarow__buybuttton"});
     buybutton.innerHTML="BUY TICKETS";
+    buybutton.classList.add("showMain__show__box__table__datarow__buybutton"); 
     rows.appendChild(date);
     rows.appendChild(venue);
     rows.appendChild(location);
@@ -66,8 +65,6 @@ setAttributes(showContainer,{"class":"showMain__show__box"});
 showContainer.appendChild(showtable);
 mainbox.appendChild(showContainer);
 }
-
-
 
 
 function setAttributes(el,attrs){
