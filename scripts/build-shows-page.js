1dfecf7456e951a117b0b271212c72f163d2f800
albinsony95 <albinsony8@gmail.com>
2023-11-05 17:13:34 -0600

    comments cleared

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,10 +1,3 @@
-// let arrayshow=[{date:"Mon Sept 06 2021",venue:"Ronald Lane",location:"San Francisco, CA"},
-//                 {date:"Tue Sept 21 2021",venue:"Pier 3 East",location:"San Francisco, CA"},
-//                 {date:"Fri Oct 15 2021",venue:"View Lounge",location:"San Francisco, CA"},
-//                 {date:"Sat Nov 06 2021 ",venue:"Hyatt Agency",location:"San Francisco, CA"},
-//                 {date:"Fri Nov 26 2021",venue:"Moscow Center",location:"San Francisco, CA"},
-//                 {date:"Wed Dec 15 2021",venue:"Press Club",location:"San Francisco, CA"}];
-
 
 const API_KEY="46b173b5-c1d4-4189-905d-742aa79fd7d0";
 const showURL=`https://project-1-api.herokuapp.com/showdates?api_key=${API_KEY}`;
